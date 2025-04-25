81425893e6c50fd71eb479593f3aa5a1e74c19e0
Ralf Vogel <ralf@ravo-it.de>
2025-04-25 19:43:53 +0200

    Blog Anfang

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
 
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  markdown: {
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
+  },
+  integrations: [tailwind(), icon(), mdx()],
 });
